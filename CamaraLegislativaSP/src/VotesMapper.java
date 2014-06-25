4978358b07f7d762c560820f0fb7331c71c1c53a
Lucas Kanashiro <18oliveira.charles@gmail.com>
2014-06-24 22:04:00 -0300

    Implementing mapper and reducer, needs improvement

--- CamaraLegislativaSP/src/VotesMapper.java
+++ CamaraLegislativaSP/src/VotesMapper.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -9,6 +11,13 @@ public class VotesMapper extends Mapper<Text, Text, Text, Text>
 	@Override
 	public void map(Text key, Text value, Context context) throws IOException, InterruptedException
 	{
-	
+		String line = value.toString(), party;
+		Pattern pattern = Pattern.compile("Partido=\"([A-Za-z]{2,})\"");
+		Matcher matcher = pattern.matcher(line);
+		if(matcher.find())
+		{
+			party = matcher.group(1);
+			context.write(new Text(party), new Text("1"));
+		}
 	}
 }
--- CamaraLegislativaSP/src/VotesReducer.java
+++ CamaraLegislativaSP/src/VotesReducer.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.Iterator;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -8,6 +10,12 @@ public class VotesReducer extends Reducer<Text, Text, Text, Text>
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
 	{
-
+		String total = "1";
+		Iterator<Text> it = values.iterator();
+		while(it.hasNext())
+		{
+			total += it.next().toString();
+		}
+		context.write(key, new Text(total));
 	}
 }
