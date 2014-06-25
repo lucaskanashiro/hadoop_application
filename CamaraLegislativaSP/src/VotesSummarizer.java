5ca1be719d452ca0f52012093690c80fe2c779b3
Charles de Oliveira <18oliveira.charles@gmail.com>
2014-06-24 21:15:32 -0300

    Adding map and reducer structure classes

---
+++ CamaraLegislativaSP/src/VotesSummarizer.java
@@ -0,0 +1,44 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class VotesSummarizer extends Configured implements Tool
+{
+	@Override
+	public int run(String[] args) throws Exception
+	{
+		if (args.length != 2)
+		{
+			System.err.printf("Usage: %s [generic options] <input> <output>\n", getClass().getSimpleName());
+			ToolRunner.printGenericCommandUsage(System.err);
+			return -1;
+		}
+
+		Job job = Job.getInstance(getConf(), "Votes Summarizer");
+		job.setJarByClass(VotesSummarizer.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.setMapperClass(VotesMapper.class);
+		job.setReducerClass(VotesReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		int exitCode = ToolRunner.run(new VotesSummarizer(), args);
+		System.out.println(exitCode);
+	}
+}
