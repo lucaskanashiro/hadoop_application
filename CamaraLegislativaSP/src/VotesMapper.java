4579d7fb307734280ce3a1bebcde9b6756e73e41
Lucas Kanashiro <alinegsantoss@gmail.com>
2014-06-26 17:29:01 -0300

    Changing Text to LongWritable in Mapper

--- CamaraLegislativaSP/src/VotesMapper.java
+++ CamaraLegislativaSP/src/VotesMapper.java
@@ -2,14 +2,15 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
-public class VotesMapper extends Mapper<Text, Text, Text, Text>
+public class VotesMapper extends Mapper<LongWritable, Text, Text, Text>
 {
 	@Override
-	public void map(Text key, Text value, Context context) throws IOException, InterruptedException
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
 	{
 		String line = value.toString(), party = "", vote = "";
 		
